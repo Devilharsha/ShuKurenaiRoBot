e17ba9e09ae65ed4dd0466f1b821ceedb89674a7
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-26 05:37:52 +0530

    Update logomaker.py

--- ShuKurenaiXRoBot/modules/logomaker.py
+++ ShuKurenaiXRoBot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ShuKurenaiXRoBot](https://t.me/ShuKurenaiXRoBot)",
+            caption="Logo by [ShuKurenaiXProBot](https://t.me/ShuKurenaiXProBot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ShuKurenaiXRoBot](https://t.me/ShuKurenaiXRoBot)",
+            caption="Logo by [ShuKurenaiXProBot](https://t.me/ShuKurenaiXProBot)",
             force_document=False,
         )
         os.remove(flnme)
